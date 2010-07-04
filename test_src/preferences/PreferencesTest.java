52d14f0ce7e403e32b4552384ed310777ddf5662
Igor Popov <igor.popov89@gmail.com>
2010-07-04 12:52:41 +0300

    Refactored the preferences test and added a new test for non-existent key names
    Signed-off-by: Igor Popov <igor.popov89@gmail.com>

--- test_src/preferences/PreferencesTest.java
+++ test_src/preferences/PreferencesTest.java
@@ -1,25 +1,43 @@
 package preferences;
 
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 import junit.framework.TestCase;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PreferencesTest extends TestCase {
 
-  @Test
-  public void testCreation() {
-    Preferences preferences = Preferences.userNodeForPackage( this.getClass() );
+  private Preferences _preferences;
+
+  @Before
+  @Override
+  public void setUp() {
+    _preferences = Preferences.userNodeForPackage( this.getClass() );
+  }
+
+  @After
+  @Override
+  public void tearDown() throws BackingStoreException {
+    _preferences.removeNode();
   }
 
   @Test
-  public void testWritingAndReadingAnInt() {
-    Preferences preferences = Preferences.userNodeForPackage( this.getClass() );
-    int intValueForPreferencesTest = 5;
-    String preferencesFieldName = "IntValueForPreferencesTest";
-    preferences.putInt( preferencesFieldName, intValueForPreferencesTest );
+  public void testWritingAndReadingAnIntWhenKeyExists() {
+    int intValue = 5;
+    int defaultValue = 0;
+    String keyName = "IntValueForPreferencesTest";
+    _preferences.putInt( keyName, intValue );
+    assertEquals( intValue, _preferences.getInt( keyName, defaultValue ) );
+  }
 
-    assertEquals( intValueForPreferencesTest, preferences.getInt( preferencesFieldName, 0 ) );
+  @Test
+  public void testReadingAnIntWhenKeyDoesntExist() {
+    int defaultValue = 0;
+    String keyName = "IntValueForPreferencesTest";
+    assertEquals( defaultValue, _preferences.getInt( keyName, defaultValue ) );
   }
 }
