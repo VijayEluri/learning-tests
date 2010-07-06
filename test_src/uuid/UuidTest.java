a74fbd38918285e6aab20c2fc76f529c19ffb2da
Igor Popov <igor.popov89@gmail.com>
2010-07-06 09:45:16 +0300

    Added a new test for random identifiers (UUIDs)
    Signed-off-by: Igor Popov <igor.popov89@gmail.com>

---
+++ test_src/uuid/UuidTest.java
@@ -0,0 +1,29 @@
+package uuid;
+
+import java.util.UUID;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class UuidTest extends TestCase {
+
+  @Test
+  public void testCreation() {
+    UUID uuid = new UUID( Long.MAX_VALUE, Long.MIN_VALUE );
+  }
+
+  @Test
+  public void testRandomIdentifier() {
+    UUID uuid1 = UUID.randomUUID();
+    UUID uuid2 = UUID.randomUUID();
+    assertTrue( !uuid1.equals( uuid2 ) );
+  }
+
+  @Test
+  public void testCompareIdentifierWithString() {
+    UUID uuid = UUID.randomUUID();
+    String uuidString = uuid.toString();
+    assertEquals( uuid, UUID.fromString( uuidString ) );
+  }
+}
