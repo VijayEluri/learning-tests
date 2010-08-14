11e06a656f95dbb4e4cddd0aff87549e57374881
Igor Popov <igor.popov89@gmail.com>
2010-08-14 16:56:17 +0300

    Discovered a bug in the File.delete method
    Added a comment describing the problem.
    
    Signed-off-by: Igor Popov <igor.popov89@gmail.com>

--- test_src/file/FileTest.java
+++ test_src/file/FileTest.java
@@ -1,43 +1,42 @@
 package file;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import java.io.*;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.*;
 
 public class FileTest {
 
-  private File        tempTestingFile;
-  private InputStream tempInputStream;
+	private File tempTestingFile;
+	private InputStream tempInputStream;
 
-  @Before
-  public void createFile() throws Exception {
-    tempTestingFile = new File( "testFile.txt" );
-    tempTestingFile.createNewFile();
-  }
+	@Before
+	public void createFile() throws Exception {
+		tempTestingFile = new File("testFile.txt");
+		tempTestingFile.createNewFile();
+	}
 
-  @After
-  public void deleteFile() throws Exception {
-    if ( tempInputStream != null ) tempInputStream.close();
-    tempTestingFile.delete();
-  }
+	@After
+	public void deleteFile() throws Exception {
+		if (tempInputStream != null)
+			tempInputStream.close();
+		tempTestingFile.delete();
+	}
 
-  @Test
-  public void deleteFileWhenFileIsClosedShouldWork() throws Exception {
-    boolean successfullyDeleted = tempTestingFile.delete();
-    assertTrue( successfullyDeleted );
-  }
+	@Test
+	public void deleteFileWhenFileIsClosedShouldWork() throws Exception {
+		boolean successfullyDeleted = tempTestingFile.delete();
+		assertTrue(successfullyDeleted);
+	}
 
-  @Test
-  public void deleteFileWhenFileIsOpenShouldNotWork() throws Exception {
-    tempInputStream = new FileInputStream( tempTestingFile );
-    boolean successfullyDeleted = tempTestingFile.delete();
-    assertFalse( successfullyDeleted );
-  }
+	@Test
+	public void deleteFileWhenFileIsOpenShouldNotWork() throws Exception {
+		tempInputStream = new FileInputStream(tempTestingFile);
+		boolean successfullyDeleted = tempTestingFile.delete();
+		// This assertion will fail on *nix (in Windows works fine, though)
+		// For more info about this bug see the following link:
+		// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4722539
+		assertFalse(successfullyDeleted);
+	}
 }
